1f0fb7803d6c700e06d697d6791f2676a9137689
Gabriel Souza da Costa <124728028+Gabrielsc19@users.noreply.github.com>
2023-06-27 15:12:02 -0300

    uyrm
    ujru

--- ESC-src/model/create-db.js
+++ ESC-src/model/create-db.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const sequelize = require('./connection-db');
 const database = require('./connection-db');
 
 const Usuario = database.define('usuario', {
@@ -29,3 +30,30 @@ const Usuario = database.define('usuario', {
     });
 
 module.exports = Usuario;
+
+const Caracteristicas = database.define('caracteristicas',{
+    idCarac: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    tipo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    descriCarac: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+});
+    Caracteristicas.belongsTo(Tarefas,{
+        constarint:true,
+        foreignKey: 'idTarefas'
+    })
+
+    Tarefas,hasMany(Tarefas,{
+        foreignKey: 'idTarefas'
+    })
+
+module.exports = Caracteristicas;
