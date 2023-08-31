cec5b3592e7d048fdbceaf9b858c7b310dbd4541
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-08-31 16:59:12 -0300

    ta
    agora deu certo

--- ESC-src/control/loginCtrl.js
+++ ESC-src/control/loginCtrl.js
@@ -11,27 +11,33 @@ exports.loginPost = async (req, res) => {
     const senha = req.body.senha;
     console.log('conxao efetuada',email,senha);
 
-    connection.query('SELECT * FROM usuarios WHERE email = ? AND senha = ?', [email, senha], (error, results, fields) => {
+    connection.query('SELECT * FROM usuario WHERE email = ? AND senha = ?', [email, senha], (error, results, fields) => {
+      console.log('pós slect');
       if (error) {
         // se ocorrer um erro, exibir mensagem de erro
         res.render('telaLogin', { error: 'Ocorreu um erro ao fazer login. Tente novamente.', errado: true});
         
-        console.log('aqui passou');
+        console.log('1 if');
+        console.log(error);
+        console.log(results);
+        console.log(fields);
 
       } else if (results.length === 0) {
         // se não houver resultados, exibir mensagem de erro
         res.render('telaLogin', { error: 'Email ou senha inválidos.', errado: true});
         
-        console.log('se chegar aqui ta errado')
+        console.log('2 if')
       } else {
         // se o usuário existir, redirecionar para o menu principal
         req.session.user = results;
-        console.log('passoutamemem');
+        console.log('3 if');
 
         res.redirect('/principal')
-        email = results[0].email;
+        nome = results[0].nome;
         console.log(results[0].nome);
        
       }
     });
+
+    console.log('pós slect finalzao');
   }
