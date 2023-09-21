7c06a9853d161ce1aea43991b8e16f3ceb138d00
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-09-21 11:17:33 -0300

    implementei a extracao porem tenho qeu fazer alguns ajustes

--- ESC-src/control/processaCtrl.js
+++ ESC-src/control/processaCtrl.js
@@ -1,27 +1,20 @@
-const connection = require('../model/conexao');
-const md5 = require('md5');     
+const md5 = require('md5');
 
 exports.processaPagina = (req, res) => {
-    res.render('telaProcessamento', {errado: false});
+    res.render('telaProcessamento', { errado: false });
 }
 
 exports.processaPost = async (req, res) => {
     const inputCaracteristicas = req.body.inputCaracteristicas;
-    const spanCarac = req.body.spanCarac
-    console.log('caracteristicas extraidas',spanCarac,inputCaracteristicas)
+    const spanCarac = req.body.spanCarac;
+    console.log('características extraídas', spanCarac, inputCaracteristicas);
 
-    if(error){
-        res.render('processamento',{error: 'Ocorreu um erro ao fazer o processamento', errado:true});
+    // Supondo que você queira verificar algum erro aqui. Defina a variável "error" adequadamente.
+
+    if (error) {
+        res.render('processamento', { error: 'Ocorreu um erro ao fazer o processamento', errado: true });
+    } else {
+        // Lógica de processamento bem-sucedida.
+        res.render('resultado', { resultado: 'Processamento bem-sucedido' });
     }
-
-    const frm = document.querySelector("#frmProcessa")
-
-    frm.addEventListener("submit",(e)=>{
-       
-    })
-
-
-
-
-
 }
