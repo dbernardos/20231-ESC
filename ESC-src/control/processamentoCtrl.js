4aa02b0166a7e004422b78dfe0102d4d4c87ec36
dbernardos <121075844+dbernardos@users.noreply.github.com>
2023-10-08 22:27:05 -0300

    chamando post no processamento (erro ainda)

--- ESC-src/control/processamentoCtrl.js
+++ ESC-src/control/processamentoCtrl.js
@@ -1,14 +1,8 @@
 const connection = require('../model/conexao');     
 
 exports.processamentoPagina = (req, res) => {
-    console.log('ESTOU NO PROCESSAMENTO GET');
-    const nomesCaracteristicas = req.body.nomesCaracteristicasInput;
-    const contagensCaracteristicas = req.body.contagensCaracteristicasInput;
-    console.log('Nomes de Características:', nomesCaracteristicas);
-
     res.render('telaProcessamento', {errado: false});
 }
-
 
 /**tentando implementar a rota com os vetores */
 exports.processamentoPost = (req, res) => {
