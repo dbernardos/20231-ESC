907bcf716d770b07d389ee499d8c1d99fc1cc0e6
Gabriel Souza da Costa <124728028+Gabrielsc19@users.noreply.github.com>
2023-10-10 16:52:38 -0300

    AA
    AAA

--- ESC-src/control/principalCtrl.js
+++ ESC-src/control/principalCtrl.js
@@ -3,5 +3,5 @@ const connection = require('../model/conexao');
 exports.principalPagina = (req, res) => {
     const usuario = req.session.user;
     console.log('Conexão efetuada (principal):', usuario[0].nome);
-    res.render('telaPrincipal', { usuario  });
+    res.render('telaPrincipal', { errorMessage: '' });
 }
