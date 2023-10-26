30eaf5af3646a41e9055f2d1179f67812c50d21d
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-10-26 17:21:31 -0300

    aa
    aa

--- ESC-src/control/processamentoCtrl.js
+++ ESC-src/control/processamentoCtrl.js
@@ -11,10 +11,11 @@ exports.processamentoPost = (req, res) => {
       // Obter os valores de nomesCaracteristicas e contagensCaracteristicas do corpo da solicitação
       const nomesCaracteristicas = req.body.nomesCaracteristicasInput;
       const contagensCaracteristicas = req.body.contagensCaracteristicasInput;
+      const nomeArquivo = req.body.nomeArquivoInput;
       // Agora você tem acesso a esses valores e pode usá-los como desejar
       console.log('Nomes de Características:', nomesCaracteristicas);
       console.log('Contagens de Características:', contagensCaracteristicas);
-      res.render('telaRelatorio', { chars: nomesCaracteristicas, contChars: contagensCaracteristicas });
+      res.render('telaRelatorio', { chars: nomesCaracteristicas, contChars: contagensCaracteristicas, nomesArquivos: nomeArquivo  });
       
     } catch (error){
       // Lida com exceções ao analisar JSON
